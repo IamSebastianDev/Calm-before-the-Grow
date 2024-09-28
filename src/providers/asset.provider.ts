2e288952a5201013b74e1b56e0f3ac7ca68e2ba0
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-09-28 19:22:21 +0200

    chore: setup primitive asset provider

---
+++ src/providers/asset.provider.ts
@@ -0,0 +1,13 @@
+/** @format */
+import selector from '../assets/sprites/selector.png';
+import tile_001 from '../assets/sprites/tile_001.png';
+import { createAssetProvider } from '../core/asset-loader';
+
+export const assets = {
+    selector,
+    dirt_1: tile_001,
+};
+
+export type AssetType = keyof typeof assets;
+
+export const { useAssets, AssetProvider } = createAssetProvider(assets);
