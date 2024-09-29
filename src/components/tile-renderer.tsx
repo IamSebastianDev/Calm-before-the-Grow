b22cf4102b7fc3d145ddc2de6d7a6b5ee6945ea9
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-09-29 14:55:45 +0200

    fix: make position required

--- src/components/tile-renderer.tsx
+++ src/components/tile-renderer.tsx
@@ -1,12 +1,14 @@
 /** @format */
 
 import { MeshProps } from '@react-three/fiber';
+import { Vector3 } from 'three';
 import { Tile } from '../stores/grid/grid.tiles';
 import { AnimatedSprite } from './animated-sprite';
 import { StaticSprite } from './static-sprite';
 
 export type TileRendererProps = MeshProps & {
     tile: Tile;
+    position: Vector3;
 };
 export const TileRenderer = ({ tile, ...props }: TileRendererProps) => {
     return Tile.isAnimatedTile(tile) ? (
