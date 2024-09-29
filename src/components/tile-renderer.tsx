b08e1a460880b5f0e52fd317fb47175aa609908a
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-09-29 10:43:10 +0200

    fix: use correct base props

--- src/components/tile-renderer.tsx
+++ src/components/tile-renderer.tsx
@@ -1,11 +1,11 @@
 /** @format */
 
-import { SpriteProps } from '@react-three/fiber';
+import { MeshProps } from '@react-three/fiber';
 import { Tile } from '../stores/grid/grid.tiles';
 import { AnimatedSprite } from './animated-sprite';
 import { StaticSprite } from './static-sprite';
 
-export type TileRendererProps = SpriteProps & {
+export type TileRendererProps = MeshProps & {
     tile: Tile;
 };
 export const TileRenderer = ({ tile, ...props }: TileRendererProps) => {
